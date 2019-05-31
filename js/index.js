1167547fdf5f4e592db5de83def4434f3b55dfb0
Liliya <liljsik1@gmail.com>
2019-05-31 20:46:11 +0300

    1

--- js/index.js
+++ js/index.js
@@ -1,37 +1,3 @@
-/*let registry = document.querySelector('.registry');
-
-function RemoveItem(e) {
-    //console.log(this.dataset.id);
-    localStorage.removeItem(`item_${this.dataset.id}`);
-    this.remove();
-    //console.log(registry.childElementCount);
-    if (registry.childElementCount === 0){
-        document.querySelector('.load-data').disabled = false;
-    }
-}
-
-function Request(){
-    let url = "https://jsonplaceholder.typicode.com/users";
-    fetch(url)
-        .then(response =>{
-            console.log("Response: ", response);
-            return response.json()
-        })
-        .then(data =>{
-            console.log("Data: ", data);
-            registry.innerHTML = '';
-            let elem = data.map(item =>{
-                localStorage.setItem(`item_${item.id}`, item);
-                let li = document.createElement('li');
-                li.setAttribute('data-id', item.id);
-                li.addEventListener('click', RemoveItem);
-                li.innerHTML = `${item.id}, ${item.name}, ${item.email}`;
-                registry.appendChild(li);
-                return item;
-            });
-            document.querySelector('.load-data').disabled = true;
-        })
-}*/
 
 class Calculator {
     constructor(x,y) {
@@ -51,8 +17,6 @@ class Calculator {
         return x / y;
     }
 }
-console.log(this);
-
 
 function calc(operation) {
     let x = document.getElementById('input1').value;
@@ -61,6 +25,6 @@ function calc(operation) {
     calc.operation();
 }
 
-
+calc[operation]();
 
 
